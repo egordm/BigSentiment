8535b078468ca71cef8443292c5c4b54f15509ff
Egor Dmitriev <egordmitriev2@gmail.com>
2021-02-27 12:54:51 +0100

    Improved electra train script

--- models/ElectraPretrainingModel.py
+++ models/ElectraPretrainingModel.py
@@ -1,9 +1,10 @@
 import os
-from typing import Union, List, Optional
+from dataclasses import dataclass
+from typing import Union, List, Optional, Tuple, Dict, Set
 
 import torch
-import wandb
-from transformers import PreTrainedModel, PreTrainedTokenizer
+# import wandb
+from transformers import PreTrainedModel, PreTrainedTokenizer, PreTrainedTokenizerBase
 
 
 class ElectraPretrainingModel(torch.nn.Module):
@@ -18,7 +19,7 @@ class ElectraPretrainingModel(torch.nn.Module):
     ):
         super().__init__()
         self.device = device
-        self.loss_weights = loss_weights if loss_weights else [1, 10]
+        self.loss_weights = loss_weights if loss_weights else [1, 50]
 
         # Initialize tokenizer and ignored token ids
         self.tokenizer = tokenizer
@@ -36,7 +37,7 @@ class ElectraPretrainingModel(torch.nn.Module):
             input_ids=None,
             attention_mask=None,
             labels=None,
-            special_tokens_mask=None,
+            masked_indices=None,
             token_type_ids=None,
             position_ids=None,
             head_mask=None,
@@ -46,24 +47,24 @@ class ElectraPretrainingModel(torch.nn.Module):
         input_ids = input_ids.to(self.device)
         attention_mask = attention_mask.to(self.device)
         labels = labels.to(self.device)
+        masked_indices = masked_indices.to(self.device)
 
+        # Create labels for the generator
+        generator_labels = labels.clone()
+        generator_labels[~masked_indices] = -100
         # Train the generator to predict the sentence / fill the masked spots
         generator_loss, generator_output = self.generator(
-            input_ids,
-            attention_mask,
-            token_type_ids,
-            position_ids,
-            head_mask,
-            position_ids,
-            labels=labels
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            labels=generator_labels
         )[:2]
 
         # Replace the masked tokens with generated values
-        fake_labels = (labels > 0).type(torch.float32)
-        masked_lm_positions = torch.nonzero(fake_labels.view(-1)).squeeze()
+        masked_lm_positions = torch.nonzero(masked_indices.view(-1)).squeeze()
         fake_logits = generator_output.view((batch_size * sequence_len, -1))[masked_lm_positions]
         fake_argmaxes = fake_logits.argmax(-1)
         fake_tokens = input_ids.view(-1).scatter(-1, masked_lm_positions, fake_argmaxes).view((batch_size, sequence_len))
+        fake_labels = (~torch.eq(labels, fake_tokens)).type(torch.float32)
 
         # Train discriminator to predict which positions are bad
         discriminator_loss, discriminator_output = self.discriminator(
@@ -79,7 +80,9 @@ class ElectraPretrainingModel(torch.nn.Module):
         total_loss = generator_loss * self.loss_weights[0] + discriminator_loss * self.loss_weights[1]
 
         # Log some extras
-        wandb.log({'discriminator_loss': discriminator_loss, 'generator_loss': generator_loss, 'total_loss': total_loss})
+        u = 0
+        # wandb.log({'discriminator_loss': discriminator_loss, 'generator_loss': generator_loss, 'total_loss': total_loss})
+        print(total_loss)
         return (
             total_loss,
             (discriminator_predictions, generator_output),
@@ -98,3 +101,37 @@ class ElectraPretrainingModel(torch.nn.Module):
 
         self.generator.save_pretrained(generator_path)
         self.discriminator.save_pretrained(discriminator_path)
+
+
+@dataclass
+class DataCollatorForElectra:
+    tokenizer: PreTrainedTokenizerBase
+    mlm: bool = True
+    mlm_probability: float = 0.15
+    special_tokens: Set[int] = None
+
+    def __post_init__(self):
+        if self.special_tokens is None:
+            self.special_tokens = set(self.tokenizer.all_special_ids)
+
+    def __call__(
+            self, examples: List[Union[List[int], torch.Tensor, Dict[str, torch.Tensor]]]
+    ) -> Dict[str, torch.Tensor]:
+        batch = self.tokenizer.pad(examples, return_tensors="pt")
+
+        input_ids = batch['input_ids']
+        labels = input_ids.clone()
+
+        # We sample a few tokens in each sequence for MLM training (with probability `self.mlm_probability`)
+        probability_matrix = torch.full(labels.shape, self.mlm_probability)
+        special_tokens_mask = torch.tensor([[int(t) in self.special_tokens for t in item] for item in input_ids])
+        probability_matrix.masked_fill_(special_tokens_mask, value=0.0)
+        masked_indices = torch.bernoulli(probability_matrix).bool()
+
+        # we replace masked input tokens with tokenizer.mask_token ([MASK])
+        input_ids[masked_indices] = self.tokenizer.mask_token_id
+
+        batch['input_ids'] = input_ids
+        batch['labels'] = labels
+        batch['masked_indices'] = masked_indices
+        return batch
--- models/train_electra.py
+++ models/train_electra.py
@@ -1,15 +1,16 @@
 import os
-import wandb
+# import wandb
+from functools import partial
 
 from datasets import load_dataset
 from transformers import ElectraConfig, ElectraForMaskedLM, ElectraForPreTraining, \
     set_seed, DataCollatorForLanguageModeling, Trainer, ElectraTokenizerFast, \
     TrainingArguments, EvaluationStrategy
 from transformers.tokenization_utils_base import PaddingStrategy
-from models.ElectraPretrainingModel import ElectraPretrainingModel
+from models.ElectraPretrainingModel import ElectraPretrainingModel, DataCollatorForElectra
 
-wandb.login()
-wandb.init(project="bigbrain")
+# wandb.login()
+# wandb.init(project="bigbrain")
 
 # Initial settings
 tokenizer_custom = {
@@ -42,7 +43,7 @@ lr = 5e-4
 vocab_size = 16537
 block_size = 200
 seed = 1337
-train_batch_size = 2
+train_batch_size = 4
 eval_batch_size = 16
 
 set_seed(seed)
@@ -53,7 +54,6 @@ tokenizer.add_special_tokens({
     'additional_special_tokens': list(tokenizer_custom.values())
 })
 assert tokenizer.vocab_size == vocab_size
-tokenizer_special_ids = set(tokenizer.all_special_ids)
 
 # Load the dataset
 dataset = load_dataset("text", data_files={
@@ -64,16 +64,8 @@ dataset = load_dataset("text", data_files={
 }, cache_dir='./cache')
 
 # Preprocess / Pre-tokenize the dataset
-def tokenize_function(input):
-    res = tokenizer(
-        input['text'], truncation=True, padding=PaddingStrategy.MAX_LENGTH, max_length=seq_length,
-        return_token_type_ids=False
-    )
-    res['special_tokens_mask'] = [[(1 if t in tokenizer_special_ids else 0) for t in item] for item in res['input_ids']]
-    return res
-
-
-tokenized_datasets = dataset.shuffle().map(tokenize_function, batched=True)
+tokenize_function = partial(tokenizer, truncation=True, padding=PaddingStrategy.MAX_LENGTH, max_length=seq_length, return_token_type_ids=False)
+tokenized_datasets = dataset.shuffle().map(lambda x: tokenize_function(x['text']), batched=True)
 eval_dataset = tokenized_datasets['train'].train_test_split(test_size=100)['test']
 
 # Build the model
@@ -102,7 +94,7 @@ discriminator = ElectraForPreTraining(config=discriminator_config)
 model = ElectraPretrainingModel(discriminator, generator, tokenizer, 'cpu')
 
 # Setup training params
-data_collator = DataCollatorForLanguageModeling(
+data_collator = DataCollatorForElectra(
     tokenizer=tokenizer, mlm=True, mlm_probability=0.15
 )
 
@@ -115,11 +107,10 @@ arguments = TrainingArguments(
     learning_rate=lr,
     load_best_model_at_end=True,
     num_train_epochs=20,
-    report_to=['wandb'],
+    # report_to=['wandb'],
     per_device_train_batch_size=train_batch_size,
     per_device_eval_batch_size=eval_batch_size,
     run_name='pretrain_electra',
-    dataloader_num_workers=2,
 )
 
 # Initialize our Trainer
