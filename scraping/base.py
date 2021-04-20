540e6f3957ff1445e41b3ed2e771922f83262fac
Egor Dmitriev <egordmitriev2@gmail.com>
2021-04-20 22:02:40 +0200

    Updated tweet scraper to use selenium to record cookies

--- scraping/base.py
+++ scraping/base.py
@@ -5,7 +5,6 @@ from scraping.binance import binance
 from scraping.coinmarketcap import coinmarketcap
 from scraping.news import news
 from scraping.twitter import twitter
-from scraping.twitter_old import twitter_old
 
 
 @click.group()
@@ -14,7 +13,6 @@ def scraper():
 
 
 scraper.add_command(twitter)
-scraper.add_command(twitter_old)
 scraper.add_command(news)
 scraper.add_command(binance)
 scraper.add_command(coinmarketcap)
--- scraping/twitter.py
+++ scraping/twitter.py
@@ -1,31 +1,135 @@
-import logging
-
-import pymongo
-from tqdm import tqdm
-from twython import Twython
-from datetime import datetime
-import re
-import os
+import json
 import click
+import pymongo
+import requests
+import re
+import time
+import datetime as dt
+import urllib.parse as u
+import logging
+from selenium import webdriver
 
 from config import mongodb
+
+driver_options = webdriver.FirefoxOptions()
+driver_options.headless = True
+driver = webdriver.Firefox(firefox_options=driver_options)
+session = requests.Session()
+
+DT_FMT = '%Y-%m-%d'
+
+URL = (
+    f'https://api.twitter.com/2/search/adaptive.json?'
+    f'include_profile_interstitial_type=1'
+    f'&include_blocking=1'
+    f'&include_blocked_by=1'
+    f'&include_followed_by=1'
+    f'&include_want_retweets=1'
+    f'&include_mute_edge=1'
+    f'&include_can_dm=1'
+    f'&include_can_media_tag=1'
+    f'&skip_status=1'
+    f'&cards_platform=Web-12'
+    f'&include_cards=1'
+    f'&include_ext_alt_text=true'
+    f'&include_quote_count=true'
+    f'&include_reply_count=1'
+    f'&tweet_mode=extended'
+    f'&include_entities=true'
+    f'&include_user_entities=true'
+    f'&include_ext_media_color=true'
+    f'&include_ext_media_availability=true'
+    f'&send_error_codes=true'
+    f'&simple_quoted_tweet=true'
+    f'&query_source=typed_query'
+    f'&pc=1'
+    f'&spelling_corrections=1'
+    f'&ext=mediaStats%2ChighlightedLabel'
+    f'&count=100'
+    f'&tweet_search_mode=live'
+    '&q={query}'
+)
+
+CURSOR_RE = re.compile('"(scroll:[^"]*)"')
+
+
+def retry(fn, count=5, delay=1):
+    ex = None
+    for i in range(count):
+        try:
+            return fn()
+        except Exception as e:
+            time.sleep(delay)
+            ex = e
+    raise ex
+
+
+def update_cookies():
+    driver.get('https://twitter.com/explore')
+
+    # Update cookies
+    cookies = driver.get_cookies()
+    for cookie in cookies:
+        session.cookies.set(cookie['name'], cookie['value'])
+
+    # Update headers
+    guest_token = driver.get_cookie('gt')['value']
+    # csrf_token = driver.get_cookie('ct0')['value']
+    headers = {
+        'authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA',
+        'x-guest-token': guest_token,
+        # 'x-csrf-token': csrf_token,
+    }
+
+    return headers
+
+
+def request_content(headers, query: str, date: dt.datetime, cursor=None):
+    if cursor:
+        url = URL + '&cursor={cursor}'
+        url = url.format(query=u.quote(query), cursor=u.quote(cursor))
+    else:
+        date_filter = f'since:{date.strftime(DT_FMT)} until:{(date + dt.timedelta(days=1)).strftime(DT_FMT)}'
+        retweet_filter = '-filter:retweets AND -filter:replies'
+        query = f"'{query}' {retweet_filter} {date_filter}"
+        url = URL.format(query=u.quote(query))
+
+    response = session.get(url, headers=headers)
+    data = json.loads(response.text)
+    cursor = CURSOR_RE.search(response.text).group(1)
+    return data, cursor
+
+
+def transform_tweet(tweet):
+    return {
+        '_id': tweet['id'],
+        'text': tweet['full_text'],
+        'user_id': tweet['user_id'],
+        'follower_count': None,
+        'verified': None,
+        'created_at': dt.datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y'),
+        'retweet_count': tweet['retweet_count'],
+        'favorite_count': tweet['favorite_count'],
+        'topics': [
+            tag.lower()
+            for tag in re.findall(r"#(\w*[0-9a-zA-Z]+\w*[0-9a-zA-Z])", tweet['full_text'])
+        ]
+    }
 
 
 @click.command()
 @click.option('--query', help='Query to fetch data from twitter from', required=True)
-@click.option('--type', type=click.Choice(['popular', 'recent', 'mixed']),
-              default='mixed', help='Type of the scrape page')
-@click.option('--depth', type=int, default=20, help='Amount of pages to visit')
-def twitter(query, type, depth):
-    APP_KEY = os.getenv('TWITTER_APP_KEY')
-    APP_SECRET = os.getenv('TWITTER_APP_SECRET')
-    query += ' -filter:retweets AND -filter:replies'
-    logging.info(f'Starting Twitter scraping. Query: {query}, Depth: {depth}')
+@click.option('--since', type=str, default=None, help='Fetch data since date')
+@click.option('--until', type=str, default=None, help='Fetch data until date')
+@click.option('--depth', type=int, default=600, help='Pages depth to check')
+def twitter(query, since, until, depth):
+    since = dt.datetime.strptime(since, DT_FMT) if since else dt.datetime.now()
+    until = dt.datetime.strptime(until, DT_FMT) if until else dt.datetime.now() - dt.timedelta(days=365)
 
-    twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
-    ACCESS_TOKEN = twitter.obtain_access_token()
-    twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+    logging.info(f'Starting Historical Twitter scraping. '
+                 f'Query: {query}, From: {since.strftime(DT_FMT)} Until: {until.strftime(DT_FMT)}')
 
+    # Database config
     client = mongodb()
     scrapperdb = client['scrapper']
     output = scrapperdb['tweets']
@@ -34,39 +138,26 @@ def twitter(query, type, depth):
         ('topics', pymongo.ASCENDING)
     ])
 
-    counter = 0
-    next_id = None
-    for _ in tqdm(range(depth)):
-        data = twitter.search(
-            q=query,
-            lang='en',
-            result_type=type,
-            tweet_mode='extended',
-            count="100",
-            next_id=next_id
-        )['statuses']
+    headers, count = None, 0
+    while since > until:
+        logging.debug(f'Scraping Tweets from: {since.strftime(DT_FMT)}')
 
-        if len(data) == 0 or next_id == data[-1]['id']:
-            break
+        # Update the cookies
+        if count % 100 == 0:
+            logging.debug('Updating cookies')
+            headers = retry(update_cookies, 5, 5)
 
-        next_id = data[-1]['id']
-        for tweet in data:
-            id = tweet['id']
-            result = {
-                '_id': id,
-                'text': tweet['full_text'],
-                'user_id': tweet['user']['id'],
-                'follower_count': tweet['user']['followers_count'],
-                'verified': tweet['user']['verified'],
-                'created_at': datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y'),
-                'retweet_count': tweet['retweet_count'],
-                'favorite_count': tweet['favorite_count'],
-                'topics': [
-                    tag.lower()
-                    for tag in re.findall(r"#(\w*[0-9a-zA-Z]+\w*[0-9a-zA-Z])", tweet['full_text'])
-                ]
-            }
-            output.update({'_id': id}, result, upsert=True)
-            counter += 1
+        # Range request
+        cursor = None
+        for i in range(depth):
+            logging.debug(f'- Scraping depth: {i}')
+            data, cursor = retry(lambda: request_content(headers, query, since, cursor=cursor), count=5, delay=1)
 
-    logging.info(f'Finished scraping twitter. Results: {counter} Query: {query}, Depth: {depth}')
+            # Parse the tweets and push them to the database
+            tweets = list(map(transform_tweet, data['globalObjects']['tweets'].values()))
+            for tweet in tweets:
+                output.update({'_id': tweet['_id']}, tweet, upsert=True)
+
+        # Update counters
+        count += 1
+        since = since - dt.timedelta(days=1)
